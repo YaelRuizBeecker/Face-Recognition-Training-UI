1b7c23c45e36fb7edb8cb8df02b0c9a19a4eccef
itsyaelx <ruizyael54@gmail.com>
2023-12-07 17:00:41 -0600

    toml added

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx',
-        _redirects: './public/_redirects' // Adjust the path as needed
-      },
-    },
-  },
 })
